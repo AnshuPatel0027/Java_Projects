d90e5b9e3405ed907c965aeb703e6793cf916365
APCodes0027 <124280971+APCodes0027@users.noreply.github.com>
2023-04-08 20:56:00 +0530

    Add files via upload

---
+++ Message Encrypter Decrypter/ed.java
@@ -0,0 +1,67 @@
+import java.util.Scanner;
+
+public class ed {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int exit=0;
+        String name;
+
+        System.out.println("*******************************WELCOME TO MESSAGE ENCRYPTER -DECRYPTER*******************************");
+        System.out.println("Enter Your Name : ");
+     while(exit!=404) {
+        name = sc.nextLine();
+        System.out.println("Enter the message : ");
+        String message = sc.nextLine();
+         
+        System.out.println("Enter the shift value (1-25) : ");
+        int shift = sc.nextInt();
+
+        System.out.println("Enter '1' for encrypt and '0' decrypt: ");
+        int n = sc.nextInt();
+        
+        // Encryption
+        if (n == 1) {
+            String encryptedMessage = encrypt(message, shift);
+            System.out.println("Encrypted message : " + encryptedMessage+"\n\n");
+        }
+
+        // Decryption
+        else if (n == 0) {
+            String decryptedMessage = decrypt(message, shift);
+            System.out.println("Decrypted message: " + decryptedMessage+"\n\n");
+        }
+        System.out.print("Enter 404 to QUITE or any other key to CONTINUE : ");
+        exit = sc.nextInt();
+     }  
+    }
+
+    public static String encrypt(String message, int shift) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < message.length(); i++) {
+            char c = message.charAt(i);
+            if (Character.isLetter(c)) {
+                c = (char) (c + shift);
+                if (Character.isLowerCase(message.charAt(i)) && c > 'z' || Character.isUpperCase(message.charAt(i)) && c > 'Z') {
+                    c = (char) (c - 26);
+                }
+            }
+            sb.append(c);
+        }
+        return sb.toString();
+    }
+
+    public static String decrypt(String message, int shift) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < message.length(); i++) {
+            char c = message.charAt(i);
+            if (Character.isLetter(c)) {
+                c = (char) (c - shift);
+                if (Character.isLowerCase(message.charAt(i)) && c < 'a' || Character.isUpperCase(message.charAt(i)) && c < 'A') {
+                    c = (char) (c + 26);
+                }
+            }
+            sb.append(c);
+        }
+        return sb.toString();
+    }
+}
